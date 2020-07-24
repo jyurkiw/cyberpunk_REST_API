3612e8756bd3bd3f70aac3f3ea014b1b265b3a80
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2020-07-24 03:38:50 -0700

    Simple test route that hits the DB.

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,23 @@
 import os
 
+from dotenv import load_dotenv
 from flask import Flask
+from flask import jsonify
+from CP2020_Discord_Bot_API.api.equipment import WeaponsRoller
 
 
 def createApp(test_config=None):
+    load_dotenv()
+    dbName = os.getenv("DB_NAME")
+
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
-    @app.route("/hello")
-    def hello():
-        return "hello world"
+    # create rollers
+    weaponsRoller = weaponsRoller(dbName)
+
+    @app.route("/weapons/categories")
+    def getWeaponCategories():
+        return jsonify(weaponsRoller.getWeaponCategories())
 
     return app
